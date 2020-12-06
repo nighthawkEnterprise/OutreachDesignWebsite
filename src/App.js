156a32d5e4f7ee3d6ab41e29c2ccae428b61c65f
simret <simretdbelay@gmail.com>
2020-12-06 14:56:41 -0800

    Made Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Simret from './component/sim';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Outreach Design Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Simret/>
+
     </div>
   );
 }
