7577920536cc585827b854089265657292aff621
Riyad191 <alsalemi1987@gmail.com>
2020-12-06 14:49:59 -0800

    Add Riyadh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Nithin from './components/Nithin';
+import Nithin from "./components/Nithin";
+import Riyadh from "./components/Riyadh";
 function App() {
   return (
     <div>
-     <h1> App </h1>
-     <Nithin />
+      <h1> App </h1>
+      <Nithin />
+      <Riyadh />
     </div>
   );
 }
