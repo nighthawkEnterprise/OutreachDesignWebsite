5d26e0e7cb25378945e7ebd0887fb9fe22bbc818
emmanuel <lubenruben2@gmail.com>
2021-01-14 23:37:50 -0800

    update

--- src/components/MeetUs/MeetUs.jsx
+++ src/components/MeetUs/MeetUs.jsx
@@ -15,7 +15,6 @@ function Meetteam() {
               <img src="../img/person.PNG" className="team_photo" />
             </div>
             <div className="team_bio_parent">
-
               <div className="empty"></div>
 
               <h2 className="team_name">Nithin</h2>
@@ -32,17 +31,16 @@ function Meetteam() {
           </div>
           <div className="team_contain">
             <div className="team_photo_parent">
-              <img src="../img/person.PNG" className="team_photo" />
+              <img src="../img/profile2.jpeg" className="team_photo" />
             </div>
             <div className="team_bio_parent">
               <div className="empty"></div>
-              <h2 className="team_name">Emmanueal</h2>
+              <h2 className="team_name">Emmanuel</h2>
               <div className="team_bio">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam,
-                  exercitationem. Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Nam, exercitationem. adipisicing elit. Nam,
-                  exercitationem. adipisicing
+                  Emmanuel is a Web Developer at Outreach Design, he holds a
+                  Bachelors Degree in Finance and Accounting. When he is not
+                  coding he enjoys reading and researching.
                 </p>
               </div>
               <a href="" className="fa fa-linkedin"></a>
@@ -54,13 +52,14 @@ function Meetteam() {
             </div>
             <div className="team_bio_parent">
               <div className="empty"></div>
-              <h2 className="team_name">Simuret</h2>
+              <h2 className="team_name">Simret</h2>
               <div className="team_bio">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam,
-                  exercitationem. Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Nam, exercitationem. adipisicing elit. Nam,
-                  exercitationem. adipisicing
+                  Simret is a full stack web developer at Outreach Design, she
+                  is a code coach at the Coder School and Teaching Assistance at
+                  Skillspire coding boot camp. She holds an Associate Degree in
+                  Information technology. When she is not coding, she enjoys
+                  watching comedy shows and family gatherings.
                 </p>
               </div>
               <a href="" className="fa fa-linkedin"></a>
@@ -68,17 +67,19 @@ function Meetteam() {
           </div>
           <div className="team_contain">
             <div className="team_photo_parent">
-              <img src="../img/person.PNG" className="team_photo" />
+              <img src="../img/riyadh.PNG" className="team_photo" />
             </div>
             <div className="team_bio_parent">
               <div className="empty"></div>
               <h2 className="team_name">Riyadh</h2>
               <div className="team_bio">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam,
-                  exercitationem. Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Nam, exercitationem. adipisicing elit. Nam,
-                  exercitationem. adipisicing
+                  Riyadh is a Web Developer at Outreach Design. He holds a
+                  Bachelors Degree in Civil Engineer under his belt with many
+                  years of experience on the field. When he is not coding or
+                  building amazing enginerring project, he spend his time
+                  playing Soccer which he is also good at in a proffesional
+                  scale.
                 </p>
               </div>
               <a href="" className="fa fa-linkedin"></a>
