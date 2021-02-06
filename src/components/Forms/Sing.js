597609bf904cbfba7bb6a41fd2cdde055ce9e857
Riyadh Alsalemi <62153882+Riyad191@users.noreply.github.com>
2021-02-06 11:34:40 -0800

    signPageUpdate

--- src/components/Forms/Sing.js
+++ src/components/Forms/Sing.js
@@ -3,10 +3,8 @@ import "./signin.css";
 function Signin() {
   return (
     <>
-      <i class="fa fa-envelope icon"></i>
       <label for="username">USERNAME</label>
       <input type="text" id="username" />
-      <i class="fa fa-key icon"></i>
       <label for="password">PASSWORD</label>
       <input type="password" id="password" />
       <input type="submit" value="submit" className="submit" />
